10a1320e4c25d6bd74b636cf13ea8faf9af4de47
Isak La Fleur <isak.engdahl@gmail.com>
2017-03-14 11:38:56 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,20 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { SearchComponent } from './components/search/search.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     SearchComponent,
-    AboutComponent
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
 
+import { SpotifyService } from '../../services/spotify.service';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html'
 })
 export class SearchComponent {
+  searchStr:string;
+
+  constructor(private _spotifyService: SpotifyService) {
+
+  }
+
+  searchMusic() {
+    //console.log(this.searchStr)
+    this._spotifyService.searchMusic(this.searchStr).subscribe(res => {
+      console.log(res.artists.items);
+    })
+
+
+
+  }
 }
